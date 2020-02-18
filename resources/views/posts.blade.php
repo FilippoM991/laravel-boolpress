9e2204b8da81747361b60c2fdd211665320b4ff0
Maran Filippo <filippo.maran.fm91@gmail.com>
2020-02-19 00:03:04 +0100

    fatto quello visto in classe, da risolvere un problema di visualizzazione

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.public')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>Lista pubblica posts</h1>
+                <ul>
+                    @forelse ($posts as $post)
+                        <li><a href="{{ route('blog.show', ['slug' => $post->slug ])}}">{{$post->title}}</a></li>
+                    @empty
+                        <li>Non ci sono ancora post</li>
+                    @endforelse
+                </ul>
+            </div>
+
+        </div>
+
+    </div>
+
+@endsection
